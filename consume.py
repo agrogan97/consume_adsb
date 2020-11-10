4bc5b6a16bcade41a4adc45b95b79a5a1fc0dd69
Alex Grogan <agrogan097@gmail.com>
2020-11-10 15:58:11 +0000

    Message data types

--- consume.py
+++ consume.py
@@ -1,13 +1,27 @@
 import json
 from kafka import KafkaConsumer
 
+
+def format_dict():
+    '''
+    Summary:
+            - Take the message and format into a dict of structure {'flight_id' : {'lat' : lat, 'long' : long}, ... }
+    '''
+
+
+
 def main():
 
     consumer = KafkaConsumer('adsb-data', bootstrap_servers=['my-cluster-kafka-bootstrap:9092'], auto_offset_reset='earliest')
 
     for message in consumer:
         print("NEW MESSAGE")
-        print(message)
+        # print(message)
+
+        # message.value gives us the value portion of each message
+        for entry in message.value:
+            print(entry)
+            print(type(entry))
 
     return None
 
